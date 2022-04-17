d0da39bed7849f3958f6caa0a68c439a3e185c69
sebastianfrueh <sfrueh@gmx.net>
2022-04-17 16:19:04 +0200

    Add playRound function with game logic and counter

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+const playerSelection = "ROCK";
+const computerSelection = computerPlay(); 
+let playerCounter = 0;
+let computerCounter = 0; 
+
+
 function computerPlay () {
     const choices = ["ROCK", "PAPER", "SCISSORS"];
     return choices[Math.floor(Math.random() * 3 )];
 }
 
-
+function playRound (playerSelection, computerSelection) {
+    switch(true) {
+        case (playerSelection === computerSelection):
+            return `Draw!`;
+        case (playerSelection === "ROCK" && computerSelection === "SCISSORS"):
+        case (playerSelection === "SCISSORS" && computerSelection === "PAPER"):
+        case (playerSelection === "PAPER" && computerSelection === "ROCK"):
+            playerCounter++; 
+            return `You won! ${playerSelection} defeats ${computerSelection}`;
+        default:
+            computerCounter++;
+            return `You lose! ${playerSelection} is defeated by ${computerSelection}.`;
+    }
+}
+console.log(playRound(playerSelection, computerSelection), playerCounter, computerCounter); 
