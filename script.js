bcf12a43eba6fa89ddcab95e8107d2b6e2d3b127
sebastianfrueh <sfrueh@gmx.net>
2022-04-18 17:16:40 +0200

    Remove global playerOutput variable, since not necessary

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
+// Game logic
+
 let playerCounter = 0;
 let computerCounter = 0; 
-let playerOutput; 
 
-function computerPlay () {
+function computerPlay() {
     const choices = ["ROCK", "PAPER", "SCISSORS"];
     return choices[Math.floor(Math.random() * 3 )];
 }
 
 
-function getPlayerInput () {
+function getPlayerInput() {
     let playerInput = window.prompt("What is your choice? Rock, Paper or Scissors?").toUpperCase();
     if (playerInput === "ROCK" || playerInput === "PAPER" || playerInput === "SCISSORS" ) {
-        playerOutput = playerInput; 
-        return playerOutput;
+        return playerInput; 
     } else {
         alert("Please try again, you can only enter 'Rock', 'Paper' or 'Scissors'.");
         return getPlayerInput();
     }
 }
 
-function playRound (playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
     switch(true) {
         case (playerSelection === computerSelection):
             console.log(`Draw!`);
@@ -36,14 +36,28 @@ function playRound (playerSelection, computerSelection) {
     }
 }
 
-function game () {
+function game() {
     playerCounter = 0; 
     computerCounter = 0; 
 
-    for (let i = 0; i < 5; i++){
-        playRound(getPlayerInput(), computerPlay());
-    }
+    // for (let i = 0; i < 5; i++){
+    //     playRound(getPlayerInput(), computerPlay());
+    // }
     playerCounter > computerCounter ? console.log("You won! GG WP!") : console.log("You lost! GG WP");
 }
 
-game();
+//game();
+
+
+// DOM manipulation
+
+
+/* Create three buttons, one for each selection. 
+Add an event listener to the buttons that call your playRound function with the 
+correct playerSelection every time a button is clicked.*/
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => playRound(button.id, computerPlay()));
+})
